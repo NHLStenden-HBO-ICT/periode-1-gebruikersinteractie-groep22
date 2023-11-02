0097e6daec12bc197552ca171c0215e530c99393
Alwin6 <Alwinva@xs4all.nl>
2023-11-02 21:47:00 +0100

    thing

---
+++ src/WinPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace periode_1_gebruikersinteractie_groep22.src {
+    /// <summary>
+    /// Interaction logic for WinPage.xaml
+    /// </summary>
+    public partial class WinPage : Page {
+        public WinPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Nextlevel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Restart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
